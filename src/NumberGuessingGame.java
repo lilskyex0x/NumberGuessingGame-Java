5cf19cd49df088faf6618d8d84ca63811c76d06a
lilskyex0x <lilskyex0x.apk@gmail.com>
2025-07-31 00:09:13 +0700

    Wrote literally the first line of code

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -3,7 +3,7 @@ public class NumberGuessingGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to my Number Guessing Game!");
 	}
 
 }
