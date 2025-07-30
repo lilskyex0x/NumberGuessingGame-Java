5395dfdeeb64b6a5b8ae2c921fbe67c2c3b7e528
lilskyex0x <lilskyex0x.apk@gmail.com>
2025-07-31 00:16:59 +0700

    Created user input number to show inside console

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 public class NumberGuessingGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to my Number Guessing Game!");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num = input.nextInt();
+		System.out.println("Your number: " + num);
+		
+		
 	}
 
 }
